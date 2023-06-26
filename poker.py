ca62fb253eb032e565b1a714d646bb2b09ae56a1
ibrahim Halil Yildiz <78043892+hhalilyildiz410@users.noreply.github.com>
2023-06-26 11:13:23 +0200

    Create poker.py

---
+++ poker.py
@@ -0,0 +1,59 @@
+p_cards={'P1': ['JH', '10H'], 'P2': ['QD', 'KD'], 'P3': ['QC', '5H'], 'P4': ['3D', '3S'], 'P5': ['6D', '8H'], 'P6': ['5D', '10S']}
+c_cards=['4H', 'AH', 'KH', 'QH', '9H']
+winner_set=set()
+winner_list=[]
+player_control_dic={}
+
+def result_card_dic():
+    for i,j in p_cards.items():
+            a=[]
+            a.extend(j)
+            a.extend(c_cards)
+            player_control_dic[i]=a
+    return player_control_dic
+
+# final_cards=result_card_dic()
+# print(final_cards)
+
+def one_pair():
+    result_card_dic()
+    
+    values={}
+    for player,cards in player_control_dic.items():
+        list=[]
+        for card in cards:
+            if card[:-1]=="A":
+                list.append(14)
+            
+            elif card[:-1]=="K":
+                list.append(13)
+                
+            elif card[:-1]=="Q":
+                list.append(12)
+                
+            elif card[:-1] =="J":
+                list.append(11)
+            else:
+                list.append(int(card[:-1]))
+        values[player]=sorted(list,reverse=True)
+        ###############   HER OYUNCUNUN KARTLARI SAYISAL OLARAK YAZILDI#############
+    # return values
+    
+# {'P1': [14, 13, 12, 11, 10, 9, 4],
+#  'P2': [14, 13, 13, 12, 12, 9, 4],
+#  'P3': [14, 13, 12, 12,  9, 5, 4],
+#  'P4': [14, 13, 12, 9,   4, 3, 3],
+#  'P5': [14, 13, 12, 9,   8, 6, 4],
+#  'P6': [14, 13, 12, 10,  9, 5, 4]}    
+    
+    for player,cards in values.items():
+        sayi=[]
+        for card in cards:
+            if cards.count(card)>1 and cards.count(card)<3:
+                winner_list.append(player)
+    for i in winner_list:
+        if winner_list.count(i)<3:
+            winner_set.add(i)
+            
+    return winner_set
+print(one_pair())
