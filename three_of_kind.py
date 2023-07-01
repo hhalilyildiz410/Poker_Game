9d628a94f3257ce6c1a3e3a7a832da2104970fb5
gnihalozkan <126766237+gnihalozkan@users.noreply.github.com>
2023-07-01 12:00:51 +0200

    Update three_of_kind.py

--- three_of_kind.py
+++ three_of_kind.py
@@ -31,9 +31,25 @@ def check_three_of_a_kind(cards):
 def three_of_a_kind():
     player_control_dic = result_card_dic()
     winners = []
+    highest_rank = 0
     for player, cards in player_control_dic.items():
         if check_three_of_a_kind(cards):
-            winners.append(player)
+            card_values = [card[:-1] for card in cards]
+            value_counts = {}
+            for value in card_values:
+                value_counts[value] = value_counts.get(value, 0) + 1
+            for value, count in value_counts.items():
+                if count == 3:
+                    rank = get_card_rank(value)
+                    if rank > highest_rank:
+                        highest_rank = rank
+                        winners = [player]
+                    elif rank == highest_rank:
+                        winners.append(player)
     return winners
+
+def get_card_rank(value):
+    ranks = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    return ranks.get(value, 0)
 
 print(three_of_a_kind())
