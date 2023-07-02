ed026f3202dd50a860a5c68b215bc68b20843273
NevinPBTH <126729053+NevinPBTH@users.noreply.github.com>
2023-07-02 14:25:42 +0200

    Update Two_Pair.py

--- Two_Pair.py
+++ Two_Pair.py
@@ -1,8 +1,10 @@
-p_cards={'P1': ['JH', 'JH'], 'P2': ['QD', 'AD'], 'P3': ['JC', '5H'], 'P4': ['3D', '3S'], 'P5': ['6D', '8H'], 'P6': ['5D', '10S']}
-c_cards=['4H', 'AH', 'AH', 'QH', '9H']
+p_cards={'P1': ['5H', 'QH'], 'P2': ['6H', 'AD'], 'P3': ['JC', '6H'], 'P4': ['3D', '3S'], 'P5': ['6D', '8H'], 'P6': ['5D', '10S']}
+c_cards=['JH', 'AH', 'AH', '6H', 'QH']
+
 
 player_control_dic={}
 winner_list=[]
+values={}
 def result_card_dic():
     for i,j in p_cards.items():
             a=[]
@@ -10,52 +12,63 @@ def result_card_dic():
             a.extend(c_cards)
             player_control_dic[i]=a
     return player_control_dic
-
+def card_numeric():
+    for player,cards in player_control_dic.items():
+        list=[]
+        for card in cards:
+            if card[0]=="A":
+                list.append(14)
+            
+            elif card[0]=="K":
+                list.append(13)
+                
+            elif card[0]=="Q":
+                list.append(12)
+                
+            elif card[0] =="J":
+                list.append(11)
+            else:
+                list.append(int(card[0]))
+        
+        list.sort(reverse=True)
+        values[player]=sorted(list,reverse=True)     
+    return values
+    
 #final_cards=result_card_dic()
 #print(final_cards)
 
 def two_pair():
     result_card_dic()
-    pairs=[]
-    values={}
-
-    for player,cards in player_control_dic.items():
-        list=[]
+    card_numeric()   
+    
+    pairs_dic= {}
+        
+    for player,cards in values.items():
+        double=[]
+        een=[]
+        pair=[]
         for card in cards:
-            if card[:-1]=="A":
-                list.append(14)
-
-            elif card[:-1]=="K":
-                list.append(13)
-
-            elif card[:-1]=="Q":
-                list.append(12)
-
-            elif card[:-1] =="J":
-                list.append(11)
-            else:
-                list.append(int(card[:-1]))
-
-        list.sort(reverse=True)
-        values[player]=sorted(list,reverse=True)    
-
-        pair_values = []
-
-        for i in range(len(list)-1 ):
-            if list[i]==list[i+1]:
-                    pair_values.append(list[i])
-        if len(pair_values) == 2:
-            pairs.append((player, pair_values))
-    j=1
-    en=pairs[0][1]
-    for j in range(len(pairs)-1):
-        if pairs[j][1] > en or pairs[j][1]==en :
-            en=pairs[j][1]
-            max=pairs[j]
-            j+=1
-
-    winner_list.append(max)
-
-    return winner_list  
+            count=cards.count(card)
+            if count ==2:
+                if card not in double:
+                    double.append(card)
+            if count == 1:
+                if card not in een:
+                    een.append(card)
+        if len(double)>=2:
+            pair.extend(double[:2])
+            pair.append(een[0])
+            pairs_dic[player] = pair
+    final_winner={} 
+    for player,cards in pairs_dic.items():        
+                
+        if cards == max(pairs_dic.values()):
+            final_winner[player]=cards
+            
+        else:
+            print('Two pair yok')
+        
+        
+    return  final_winner
 sonuc=two_pair()
 print(sonuc)
