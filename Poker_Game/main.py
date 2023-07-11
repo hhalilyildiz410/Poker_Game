58cd3171c2de68c801d6a7aeedc4e5c4858b7aff
ibrahim Halil Yildiz <78043892+hhalilyildiz410@users.noreply.github.com>
2023-07-11 21:45:38 +0200

    Update main.py

--- Poker_Game/main.py
+++ Poker_Game/main.py
@@ -8,12 +8,3 @@ if __name__ == '__main__':
 	winner_screen = WinnerScreen()
 	winner_screen.show()
 	sys.exit(app.exec_())
-
-
-
-
-conn = sqlite3.connect("statistic.db")
-curr=conn.cursor()
-conn.commit()
-table = curr.execute("create table if not exists urun (urunKodu int, urunAdi text, birimFiyati int)")
-conn.commit()
