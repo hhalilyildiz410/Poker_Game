58c428e0eb4251090eb8fd15be4c4503355a5c14
ibrahim Halil Yildiz <78043892+hhalilyildiz410@users.noreply.github.com>
2023-07-14 18:35:31 +0200

    Add files via upload

---
+++ Poker_Game/Database_create.py
@@ -0,0 +1,19 @@
+import sqlite3
+import itertools
+from winner_check import Game
+
+combinations = list(itertools.combinations(Game().deck_create(), 2))
+combinations = [list(combination) for combination in combinations]
+# print(sorted(combinations))
+conn = sqlite3.connect('statistic.db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS PokerStatistics
+                (hands TEXT, hand_count INT, win_count INT, winner_rate FLOAT)''')
+
+for combination in combinations:
+    hands = ' '.join(sorted(combination))
+    cursor.execute("INSERT INTO PokerStatistics (hands, hand_count, win_count,winner_rate) VALUES (?, 0, 0,0)", (hands,))
+
+conn.commit()
+conn.close()
