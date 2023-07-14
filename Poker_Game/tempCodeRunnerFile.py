9a75c6e518c36012ef61e17ee6c47b3215c4a752
ibrahim Halil Yildiz <78043892+hhalilyildiz410@users.noreply.github.com>
2023-07-14 18:29:48 +0200

    Update tempCodeRunnerFile.py

--- Poker_Game/tempCodeRunnerFile.py
+++ Poker_Game/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
 
-conn = sqlite3.connect("statistic.db")
-curr=conn.cursor()
-conn.commit()
-table = curr.execute("create table if not exists urun (urunKodu int, urunAdi text, birimFiyati int)")
-conn.commit()
